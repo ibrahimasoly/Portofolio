832e08e2f9c2fa9de5320600b01039d43c34713a
Ibrahima Coulibaly <bakcouliba@gmail.com>
2024-09-15 14:59:19 +0100

    add the components projet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,18 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { PresentationComponent } from "./components/presentation/presentation.component";
 import { AproposComponent } from "./components/apropos/apropos.component";
 import { CompetenceComponent } from "./components/competence/competence.component";
+import { ProjetComponent } from "./components/projet/projet.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet, HeaderComponent, 
-    FooterComponent, PresentationComponent, 
-    AproposComponent, CompetenceComponent
-  ],
+    RouterOutlet, HeaderComponent,
+    FooterComponent, PresentationComponent,
+    AproposComponent, CompetenceComponent,
+    ProjetComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
