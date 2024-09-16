06c4bec87fa04433c02ea9493b784371e74b8610
Ibrahima Coulibaly <bakcouliba@gmail.com>
2024-09-16 16:15:06 +0100

    add the dialog with all image in component projet

--- src/app/components/projet/projet.component.ts
+++ src/app/components/projet/projet.component.ts
@@ -5,8 +5,41 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './projet.component.html',
-  styleUrl: './projet.component.css'
+  styleUrls: ['./projet.component.css']
 })
 export class ProjetComponent {
 
+  isModalOpen = false;
+  modalImage: string = '';
+
+  projects = [
+    {
+      id:1,
+      name: 'Projet 1',
+      description: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png'
+    },
+    {
+      id:2,
+      name: 'Projet 2',
+      description: 'Description du projet 2',
+      image: 'assets/images/cabinet/img1.png'
+    },
+    {
+      id:3,
+      name: 'Projet 3',
+      description: 'Description du projet 3',
+      image: 'assets/images/cabinet/img1.png'
+    }
+  ];
+
+  openModal(image: string): void {
+    this.modalImage = image;
+    this.isModalOpen = true;
+  }
+
+  closeModal(): void {
+    this.isModalOpen = false;
+    this.modalImage = '';
+  }
 }
