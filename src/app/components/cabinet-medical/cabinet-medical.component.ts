3d21f37659c6f87be930c6900ec8e3e34686d898
Ibrahima Coulibaly <bakcouliba@gmail.com>
2024-09-17 15:59:32 +0100

    edit the component cabinet-medical

--- src/app/components/cabinet-medical/cabinet-medical.component.ts
+++ src/app/components/cabinet-medical/cabinet-medical.component.ts
@@ -8,5 +8,52 @@ import { Component } from '@angular/core';
   styleUrl: './cabinet-medical.component.css'
 })
 export class CabinetMedicalComponent {
+  isModalOpen=false;
+  modalImage:string=''
+  projects = [
+    {
+      id:1,
+      explication: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png',
+    },
+    {
+      id:2,
+      explication: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png',
+    },
+    {
+      id:3,
+      explication: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png',
+    },
+    {
+      id:4,
+      explication: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png',
+    },
+    {
+      id:5,
+      explication: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png',
+    },
+    {
+      id:6,
+      explication: 'Description du projet 1',
+      image: 'assets/images/cabinet/img1.png',
+    },
+  ];
+
+  openModal(url:string){
+    this.isModalOpen=true
+    this.modalImage=url
+  }
+
+  closeModal(){
+    this.isModalOpen=false
+    this.modalImage=''
+  }
+
+
+
 
 }
