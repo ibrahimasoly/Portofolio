45447f1211c322baa64b266a93971ba178a47554
Ibrahima Coulibaly <bakcouliba@gmail.com>
2024-09-20 10:10:24 +0100

    update the component bus-transport

--- src/app/components/bus-transport/bus-transport.component.ts
+++ src/app/components/bus-transport/bus-transport.component.ts
@@ -15,47 +15,47 @@ export class BusTransportComponent implements OnInit {
   projects = [
     {
       id:1,
-      explication: 'page de connexion en tant que medecin',
+      explication: 'Recherche des bus ',
       image: 'assets/images/bus/img1.png',
     },
     {
       id:2,
-      explication: 'Liste des patients',
+      explication: 'Choix du lieu de depart',
       image: 'assets/images/bus/img2.png',
     },
     {
       id:3,
-      explication: 'Les patient qui ont des RDVs',
+      explication: "Choix du lieu d'arriver",
       image: 'assets/images/bus/img3.png',
     },
     {
       id:4,
-      explication: 'Les patients qui ont été consulter',
+      explication: 'Choix de la date de depart',
       image: 'assets/images/bus/img4.png',
     },
     {
       id:5,
-      explication: 'les informations sur le patient consulter',
+      explication: 'Chargement',
       image: 'assets/images/bus/img5.png',
     },
     {
       id:6,
-      explication: "Creation d'ordonnance pour le patient consulter" ,
+      explication: "Liste des voyages disponible" ,
       image: 'assets/images/bus/img6.png',
     },
     {
       id:7,
-      explication: "Ajouter un patient" ,
+      explication: "Remplissage des informations du passager" ,
       image: 'assets/images/bus/img7.png',
     },
     {
       id:8,
-      explication: "Ajouter un RDV" ,
+      explication: "Payment" ,
       image: 'assets/images/bus/img8.png',
     },
     {
       id:9,
-      explication: "Ajouter une consultation" ,
+      explication: "Ticket" ,
       image: 'assets/images/bus/img9.png',
     },
   ];
@@ -63,7 +63,7 @@ export class BusTransportComponent implements OnInit {
   langages=[
     {
       id:1,
-      technologies:"Frontend : Angular(HTML5, SCSS, TypeScript), Bootstrap."
+      technologies:"Frontend : Flutter(Dart)."
     },
     {
       id:2,
@@ -81,23 +81,28 @@ export class BusTransportComponent implements OnInit {
   fonctions=[
     {
       id:1,
-      title:"Prise de rendez-vous",
-      technologies:"Prise et gestion des rendez-vous des patients, soit directement au cabinet, soit par téléphone. Les informations de la prise de rendez-vous incluent le nom, prénom, et la date et heure souhaitée, avec un ajustement selon la disponibilité du médecin."
+      title:"Réservation en ligne des billets",
+      technologies:" Permet aux utilisateurs de réserver facilement des billets de bus directement depuis leur téléphone, sans avoir à se déplacer dans une agence."
     },
     {
       id:2,
-      title:"Gestion des consultations",
-      technologies:"Ajout et gestion des consultations médicales via un formulaire interactif, comprenant l’enregistrement des nouveaux patients, la gestion des fiches médicales existantes, et la mise à jour des antécédents médicaux pour chaque consultation."
+      title:"Tableau de bord interactif",
+      technologies:"Vue d’ensemble des réservations, incluant les trajets sélectionnés, les paiements effectués, et les billets validés."
     },
     {
       id:3,
-      title:"Gestion des antécédents",
-      technologies:"Mise à jour automatique des antécédents médicaux et chirurgicaux du patient pour une traçabilité optimale lors des consultations ultérieures."
+      title:"Authentification des billets",
+      technologies:"Les billets contiennent un QR code scanné à l’agence pour vérifier leur authenticité et leur validité en temps réel."
     },
     {
       id:4,
-      title:"Gestion de la comptabilité",
-      technologies:"Ajout et gestion des recettes du cabinet, incluant les frais de consultation et les dépenses associées (achat de médicaments, factures, fournitures). Les pièces justificatives sont archivées pour garantir la transparence financière."
+      title:"Optimisation du transport",
+      technologies:"Réduction des temps d’attente et des déplacements inutiles grâce à la réservation en ligne, simplifiant ainsi l’accès aux services de transport interrégionaux au Mali."
+    },
+    {
+      id:5,
+      title:"Sécurité et fiabilité ",
+      technologies:"Transactions sécurisées et informations fiables sur les trajets, garantissant une expérience utilisateur optimale."
     },
   ];
 
