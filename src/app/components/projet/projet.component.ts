9b8fb87d0a438c82fef27183323333709511abb2
Ibrahima Coulibaly <bakcouliba@gmail.com>
2024-09-22 11:37:55 +0100

    editing the description of component projet

--- src/app/components/projet/projet.component.ts
+++ src/app/components/projet/projet.component.ts
@@ -17,26 +17,27 @@ export class ProjetComponent{
 
   isModalOpen = false;
   modalImage: string = '';
+  n=0;
 
   projects = [
     {
       id:1,
       name: 'Cabinet Medical',
-      description: 'Description du projet 1',
+      description: "L'application Cabinet-Médical permet d'aider les professionnels de santé, tels que les médecins généralistes et spécialistes, à mieux gérer leur cabinet. Elle simplifie des tâches importantes comme l'accès aux dossiers des patients, la gestion des rendez-vous, le suivi des consultations, et la communication entre les différents intervenants médicaux.",
       image: 'assets/images/cabinet/img1.png',
       lien:'cabinet'
     },
     {
       id:2,
       name: 'Bus Transport',
-      description: 'Description du projet 2',
+      description: "Cette application facilite la réservation de billets de bus en éliminant le besoin de se rendre en agence. Les utilisateurs peuvent réserver et payer leurs billets en ligne via leur téléphone, et reçoivent un ticket numérique avec un QR code pour vérifier la validité à l'arrivée.",
       image: 'assets/images/bus/img1.png',
       lien:'busTransport'
     },
     {
       id:3,
       name: 'My Logement',
-      description: 'Description du projet 3',
+      description: "My Logement permet aux utilisateurs de consulter le site et de faire des demandes immobilières en tant que clients. Les fournisseurs peuvent également s'inscrire sur la plateforme pour y ajouter les informations concernant leurs logements. L'application intègre des API d'intelligence artificielle (IA) qui vérifient l'authenticité des images fournies par les fournisseurs et confirment la présence du bâtiment à l'adresse indiquée",
       image: 'assets/images/logement/img1.png',
       lien:'myLogoment'
     }
@@ -45,11 +46,15 @@ export class ProjetComponent{
   openModal(image: string): void {
     this.modalImage = image;
     this.isModalOpen = true;
+    if(image=='assets/images/bus/img1.png'){
+      this.n=2
+    };
   }
 
   closeModal(): void {
     this.isModalOpen = false;
     this.modalImage = '';
+    this.n=0;
   }
 
   openProjet(url:string):void{
